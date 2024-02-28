e29ad7b8839598ef0314a0e7b5defe4cf8538c1a
boimgaa <161588994+boimgaa@users.noreply.github.com>
2024-02-28 18:18:34 +0700

    Delete week0-pbo-rc/week0-pbo-rc directory

--- week0-pbo-rc/week0-pbo-rc/122140159_Bonipacius_Immanuel_Sinaga_Pset{3}.py
+++
@@ -1,8 +0,0 @@
-# Membuka file Me.txt untuk menulis
-with open('Me.txt', 'w') as file:
-    # Menulis baris-baris yang diinginkan ke dalam file
-    file.write('Nama : Bonipacius Immanuel Sinaga\n')
-    file.write('NIM : 122140032\n')
-    file.write('Resolusi Saya di Tahun ini : ingin berbakti kepada nusa dan bangsa\n')
-
-print("File 'Me.txt' berhasil dibuat dan diisi dengan data.")
